5781fb7448c963cf62736488f10b265efffe04f3
Mark <mark@mark-VirtualBox.(none)>
2014-08-08 23:40:22 -0400

    added percent smoothing and plotted graph over time

--- percent_track.py
+++ percent_track.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 # hardcoded size of zero template...
@@ -61,7 +61,34 @@ def extend_locations(locations):
         extended_locations.append((location[0] - WIDTH - BUFFER_SIZE, location[1] - BUFFER_SIZE))
         extended_locations.append((location[0] - (2 * WIDTH) - BUFFER_SIZE, location[1] - BUFFER_SIZE))
     return extended_locations    
-            
+
+def calculate_total_percent(ones, tens, hundreds):
+    # calculate actual value from three digits...
+    # return -1 if percentage does not make sense.
+    if ones == -1:
+        return -1
+    total = ones;
+    if hundreds != -1:
+        total += hundreds * 100
+    if tens != -1:
+        total += tens * 10
+    return total
+
+def clean_up_data(percent_series):
+    # clean up the percentage series.  probably shouldn't believe a jump of more than 30, or a drop in percentage (unless the drop is to 0)
+    # if either of these occurs, just keep the previous data
+    cleaned_data = []
+    prev_valid_point = -1
+    for point in percent_series:
+        if point > prev_valid_point + 30:
+            cleaned_data.append(prev_valid_point)
+        elif point < prev_valid_point and point != 0:
+            cleaned_data.append(prev_valid_point)
+        else:
+            cleaned_data.append(point)
+            prev_valid_point = point
+    return cleaned_data
+
 def main():
     # if threshold greater than this, its a match
     # maybe should just take top two instead..
@@ -94,12 +121,16 @@ def main():
 
     _, previous_frame = cap.read()
     prev_stability = False
+    frames_elapsed = 0
+    percent_series_1 = []
+    percent_series_2 = []
+    time_series = []
     while(cap.isOpened()):
         ret ,frame = cap.read()
+        frames_elapsed += 1
         if ret == True:
-            cv2.imshow('frame', frame)
+            # cv2.imshow('frame', frame)
             if not compare_with_previous(previous_frame, frame, locations_found):
-                cv2.waitKey(0)
                 # percentage will shake around, making it unstable
                 # wait until stable again to look for difference between it and previous one
                 cur_stability = False
@@ -107,20 +138,37 @@ def main():
                 cur_stability = True
             # if we've stabilized, check both percentages to see whats changed
             if cur_stability and not prev_stability:
+                best_guesses = []
                 for idx, location in enumerate(extended_locations_found):
                     candidate = frame[location[1]:location[1] + HEIGHT + (2 * BUFFER_SIZE), location[0]:location[0] + WIDTH + (2 * BUFFER_SIZE)]
-                    cv2.imshow('candidate', candidate)
-                    cv2.waitKey(0)
+                    # cv2.imshow('candidate', candidate)
+                    # cv2.waitKey(0)
                     best_guess = match_to_number(candidate, number_templates)
-                    print "location: " + str(idx)
-                    print "guessed percent: " + str(best_guess)
-                    
+                    # print "location: " + str(idx)
+                    # print "guessed percent: " + str(best_guess)
+                    best_guesses.append(best_guess)
+                percent_1 = calculate_total_percent(best_guesses[0], best_guesses[1], best_guesses[2])
+                percent_2 = calculate_total_percent(best_guesses[3], best_guesses[4], best_guesses[5])
+                time_elapsed = float(frames_elapsed)/60
+                print "Location 1: " + str(percent_1) + " Location 2: " + str(percent_2) + " at time " + str(time_elapsed)
+                percent_series_1.append(percent_1)
+                percent_series_2.append(percent_2)
+                time_series.append(time_elapsed)
+                #cv2.waitKey(0)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break   
             prev_stability = cur_stability
         else:
             break
         previous_frame = frame
+    # loops has ended, clean up the data now?
+    cleaned_1 = clean_up_data(percent_series_1)
+    cleaned_2 = clean_up_data(percent_series_2)
+    plt.plot(time_series, cleaned_1, 'b-')
+    # plt.plot(time_series, percent_series_1, 'bo')
+    plt.plot(time_series, cleaned_2, 'r-')
+    # plt.plot(time_series, percent_series_2, 'ro')
+    plt.show()
     cv2.destroyAllWindows()
     cap.release()
 

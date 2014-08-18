0a348733421cfc5c20a37e0d834adff80ab1b7df
rokimomi <rokimomi@gmail.com>
2014-08-18 00:41:07 -0400

    Using "Go" to find the start of first match
    Still limited to the falcon match. Next steps: vary the size of the
    go_template in the search if matches aren't found in the first 3 minutes
    of the game.

---
+++ getGo.py
@@ -0,0 +1,21 @@
+# find Go and put it into resources
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('falconDitto.mp4')
+
+# take first frame of the video
+ret,frame = cap.read()
+
+for i in range(1, 1432):
+    cap.read()
+
+ret, frame = cap.read()
+
+go = frame[155:195,242:355]
+cv2.imshow('go_', go)
+cv2.waitKey(0)
+# confirm that zero looks correct
+cv2.imwrite('smash_resources_v2/go.png', go)
+
+

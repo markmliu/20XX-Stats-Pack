0d2f004084076c7a4c9f21129f61d176fdd66811
rokimomi <rokimomi@gmail.com>
2014-08-19 00:59:55 -0400

    initial resizing "go" search algorithm implemented
    using stab vs ppu, it was pretty fast but the falcon match, unless u
    skip the first 1400 frames like in the getresources file, ur in for a
    long wait

--- README.md
+++ README.md
@@ -2,8 +2,5 @@
 ===============
 
 Stats package for SSBM
-fork todo:
 - be able to determine location of "GO!" through image search
-- be able to then calculate percentage locations based off of "GO!" location
-- be able to determine start time of matches using "GO!" from vids of the same tournament
-- resize the "GO!" search image and be able to find it in a different sized vid of same tourney
+- for any sized image
--- getGo.py
+++ getGo.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2
 
+"""
 cap = cv2.VideoCapture('falconDitto.mp4')
 
 # take first frame of the video
@@ -13,6 +14,22 @@ for i in range(1, 1432):
 ret, frame = cap.read()
 
 go = frame[155:195,242:355]
+"""
+
+
+cap = cv2.VideoCapture('ppu-vs-stab.mp4')
+
+# take first frame of the video
+ret,frame = cap.read()
+
+for i in range(1, 70):
+    cap.read()
+
+ret, frame = cap.read()
+
+go = frame[310:385, 325:560]
+
+
 cv2.imshow('go_', go)
 cv2.waitKey(0)
 # confirm that zero looks correct
